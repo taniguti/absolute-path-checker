ee51687170e8ae95309fcc76182584077271887d
Takanori TANIGUCHI <askme@pretendco.net>
2018-12-13 22:01:52 +0900

    wip

--- apc.sh
+++ apc.sh
@@ -10,11 +10,25 @@ fi
 
 
 
-for scriptfile in "$@"
+for givenfile in "$@"
 do
-    echo "$scriptfile"
-    if [ ! -f "$scriptfile" ]; then
-        echo "$scriptfile is not found."
+    echo "$givenfile"
+    if [ ! -f "$givenfile" ]; then
+        echo "$givenfile is not found."
         continue
     fi
+    if [ "$( file "$givenfile" | grep -c "shell script text" )" -eq 1 ]; then
+        scriptfiles="$scriptfiles $givenfile"
+    else
+        echo "$givenfile is not shell script"
+        file "$givenfile"
+    fi
+done
+
+for scriptfile in $scriptfiles
+do
+    grep -v ^# "$scriptfile" | \
+        tr ' !|[];\' '\n'| \
+        sort | uniq | \
+        grep -v -e ^- -e '"' -e ^\$[a-zA-Z0-9.-]
 done
